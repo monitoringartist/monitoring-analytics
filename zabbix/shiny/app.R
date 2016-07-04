abdf7bc0f42d19912597bc65dd63d97cd49d863a
Jan Garaj <jan.garaj@gmail.com>
2016-07-04 23:06:27 +0100

    Local images

--- zabbix/shiny/app.R
+++ zabbix/shiny/app.R
@@ -32,7 +32,7 @@ ui <- shinyUI(
             "limit",
             "Last values limit",
             min = 10,
-            max = 100,
+            max = 100000,
             value = 10,
             step = 10
           )
